5f945f5095b3022dfe1c9c0dbb0d8c6024e599b6
vicanso <vicansocanbico@gmail.com>
2014-01-12 20:40:32 +0800

    commit

--- jtlazy_load.js
+++ jtlazy_load.js
@@ -1,82 +1,70 @@
 (function() {
   "use strict";
 
-  var $, exports, module, moduleName, _;
-
-  moduleName = 'jtlazy_load';
-
-  module = GLOBAL_MODULES[moduleName] = {
-    id: moduleName
-  };
-
-  exports = module.exports = {};
-
-  require('jquery');
-
-  require('underscore');
-
-  _ = window._;
-
-  $ = window.jQuery;
-
-  exports.load = function(lazyLoadItems, scrollObj) {
-    var bindingEventObj, getLazyLoadItemInfos, lazyLoadItemInfos, loadImage, windowHeight;
-    windowHeight = $(window).height();
-    bindingEventObj = scrollObj || $(window);
-    getLazyLoadItemInfos = function() {
-      var tmpScrollTop;
-      if (scrollObj) {
-        tmpScrollTop = scrollObj.scrollTop();
-      }
-      return _.compact(_.map(lazyLoadItems, function(obj) {
-        var top;
-        obj = $(obj);
-        if (obj.find('img.hidden').length) {
-          if (tmpScrollTop) {
-            top = obj.position().top + tmpScrollTop;
-          } else {
-            top = obj.offset().top;
-          }
-          return {
-            top: top,
-            obj: obj
-          };
-        } else {
-          return null;
+  define('jtLazyLoad', ['jquery', 'underscore'], function(require, exports) {
+    var load;
+    load = function(lazyLoadItems, scrollObj) {
+      var $, bindingEventObj, getLazyLoadItemInfos, lazyLoadItemInfos, loadImage, windowHeight, _;
+      $ = require('jquery');
+      _ = require('underscore');
+      windowHeight = $(window).height();
+      bindingEventObj = scrollObj || $(window);
+      getLazyLoadItemInfos = function() {
+        var tmpScrollTop;
+        if (scrollObj) {
+          tmpScrollTop = scrollObj.scrollTop();
         }
-      }));
-    };
-    lazyLoadItemInfos = getLazyLoadItemInfos();
-    loadImage = _.debounce(function() {
-      var containerObj, maxTop, top;
-      containerObj = scrollObj || $(document);
-      top = containerObj.scrollTop() - 300;
-      maxTop = top + windowHeight + 600;
-      lazyLoadItemInfos = _.filter(lazyLoadItemInfos, function(lazyLoadItemInfo) {
-        var lazyLoadItemInfoTop;
-        lazyLoadItemInfoTop = lazyLoadItemInfo.top;
-        if (lazyLoadItemInfoTop > top && lazyLoadItemInfoTop < maxTop) {
-          lazyLoadItemInfo.obj.find('img').each(function() {
-            var img, imgSrc, obj;
-            obj = $(this);
-            img = new Image();
-            imgSrc = obj.attr('data-src');
-            img.onload = function() {
-              return obj.removeClass('hidden');
+        return _.compact(_.map(lazyLoadItems, function(obj) {
+          var top;
+          obj = $(obj);
+          if (obj.find('img.hidden').length) {
+            if (tmpScrollTop) {
+              top = obj.position().top + tmpScrollTop;
+            } else {
+              top = obj.offset().top;
+            }
+            return {
+              top: top,
+              obj: obj
             };
-            img.src = imgSrc;
-            return obj.attr('src', imgSrc);
-          });
-          return false;
-        } else {
-          return true;
+          } else {
+            return null;
+          }
+        }));
+      };
+      lazyLoadItemInfos = getLazyLoadItemInfos();
+      loadImage = _.debounce(function() {
+        var containerObj, maxTop, top;
+        containerObj = scrollObj || $(document);
+        top = containerObj.scrollTop() - 300;
+        maxTop = top + windowHeight + 600;
+        lazyLoadItemInfos = _.filter(lazyLoadItemInfos, function(lazyLoadItemInfo) {
+          var lazyLoadItemInfoTop;
+          lazyLoadItemInfoTop = lazyLoadItemInfo.top;
+          if (lazyLoadItemInfoTop > top && lazyLoadItemInfoTop < maxTop) {
+            lazyLoadItemInfo.obj.find('img').each(function() {
+              var img, imgSrc, obj;
+              obj = $(this);
+              img = new Image();
+              imgSrc = obj.attr('data-src');
+              img.onload = function() {
+                return obj.removeClass('hidden');
+              };
+              img.src = imgSrc;
+              return obj.attr('src', imgSrc);
+            });
+            return false;
+          } else {
+            return true;
+          }
+        });
+        if (!lazyLoadItemInfos.length) {
+          return bindingEventObj.off('scroll', loadImage);
         }
-      });
-      if (!lazyLoadItemInfos.length) {
-        return bindingEventObj.off('scroll', loadImage);
-      }
-    }, 300);
-    return bindingEventObj.scroll(loadImage);
-  };
+      }, 300);
+      return bindingEventObj.scroll(loadImage);
+    };
+    exports.load = load;
+  });
 
 }).call(this);
